0f146494d9819235a557132f8a296670bde7e864
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-04-24 13:23:39 -0500

    functions code

--- purgeDatasetsFromGroup.py
+++ purgeDatasetsFromGroup.py
@@ -1,19 +1,43 @@
 import os
-from ckanapi import RemoteCKAN
+import ckanapi
 from dotenv import load_dotenv
 
 load_dotenv()
-
 HOST = os.getenv('CKAN_HOST')
 TOKEN = os.getenv('CKAN_API_TOKEN')
 ORG= os.getenv('ORG_TO_PURGE')
-remote = RemoteCKAN(HOST, user_agent='ckanops/3.0', apikey=TOKEN)
+
+def getOrgDatasets(remote, org):
+    try:
+        org = remote.action.organization_show(id=org, include_datasets='true')
+        return org['packages']
+    except ckanapi.NotFound:
+        print 'Not Found'
+        pass
+
+def getDataset(remote, idDataset):
+    try:
+        dataset = remote.action.package_show(id=idDataset)
+        return dataset['id']
+    except ckanapi.errors.CKANAPIError:
+        print 'Not Found'
+        pass
+
+def purgeDataset(remote, idDataset):
+    try:
+        remote.action.dataset_purge(id=idDataset)
+        return 'Deleted'
+    except ckanapi.errors.NotAuthorized:
+        print 'Not Purge'
+        pass
+
+remote = ckanapi.RemoteCKAN(HOST, user_agent='ckanops/1.0', apikey=TOKEN)
 org = remote.action.organization_show(id=ORG)
-print org
-orgDatasets = remote.action.organization_show(id=ORG,include_datasets='true')
-
-for d in orgDatasets['packages']:
-    print d['id']
-    remote.action.dataset_purge(id=d['id'])
-
-
+print org['id']
+orgDatasets = getOrgDatasets(remote, org['id'])
+print len(orgDatasets)
+for d in orgDatasets:
+    if getDataset(remote, d['id']):
+        datasetFound = getDataset(remote, d['id'])
+        print datasetFound + ' FOUND TO DELETE'
+        purgeDataset(remote, datasetFound)
