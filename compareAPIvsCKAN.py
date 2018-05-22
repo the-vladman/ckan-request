a1a032653569125c55911c13698244aaae19c06a
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-05-22 18:18:59 -0500

    update script

--- compareAPIvsCKAN.py
+++ compareAPIvsCKAN.py
@@ -2,12 +2,22 @@ import os
 import ckanapi
 from dotenv import load_dotenv
 import requests
+import csv
+import sys
 
+reload(sys)
+sys.setdefaultencoding('utf8')
 load_dotenv()
+
 CKAN_HOST = os.getenv('CKAN_HOST')
 CKAN_TOKEN = os.getenv('CKAN_API_TOKEN')
 API_URL= os.getenv('API_URL')
 COLLECTION_NAME= os.getenv('COLLECTION_NAME')
+
+file = open("comparativaCKANvsAPI.csv","w")
+fieldnames = ['ID_API', 'Resource_Name_CKAN', 'Found_CKAN']
+writer = csv.DictWriter(file, fieldnames=fieldnames)
+writer.writeheader()
 
 def getDatasetFromCKAN(remote, idDataset):
     try:
@@ -18,10 +28,10 @@ def getDatasetFromCKAN(remote, idDataset):
 
 def getResourceFromCKAN(remote, idResource):
     try:
-        dataset = remote.action.package_show(id=idResource)
-        return dataset['id']
-    except ckanapi.errors.CKANAPIError:
-        pass
+        resource = remote.action.resource_show(id=idResource)
+        return 'found'
+    except ckanapi.errors.CKANAPIError as e:
+        return 'not found'
 
 def getAPIResources(page,pageSize):
     try:
@@ -30,13 +40,29 @@ def getAPIResources(page,pageSize):
         response = requestAPI.json()
         results = response['results']
         return results
-    except:
-        print 'error'
+    except requests.exceptions as e:
+        print 'ERROR' + e
+
+totalResources = 34765
+page = 1
+pageSize = 3000
+divideTotal = 12
 
 remote = ckanapi.RemoteCKAN(CKAN_HOST, user_agent='ckanops/1.0', apikey=CKAN_TOKEN)
 
+while page < divideTotal:
+    print page
+    resourcesToVerify = getAPIResources(page, pageSize)
+    for resource in resourcesToVerify:
+        resourceToVerify = getResourceFromCKAN(remote, resource['id'])
+        nameResource = ''
+        if 'name' in resource:
+             nameResource = resource['name']
+        else:
+            nameResource = 'Sin nombre'
+        row = {'ID_API': resource['id'], 'Resource_Name_CKAN': nameResource, 'Found_CKAN':resourceToVerify }
+        writer.writerow(row)
+    page = page + 1
+    print page
 
-
-resourcesToVerify = getAPIResources(1, 1000)
-
-print len(resourcesToVerify)
+file.close()
